5271fb31efd7ed110dacb95ef0b93a3fa032c019
cstitt1 <cstitt1@villanova.edu>
2019-11-26 11:55:24 -0500

    Testing branches with git

--- c_code/hello2.c
+++ c_code/hello2.c
@@ -2,10 +2,9 @@
 #include <sys/shm.h> 
 #include <stdio.h> 
 #include <string.h>
-//git branch testing comment
 
 int main() 
 {
-	printf("Hello world!");
+	printf("Hello world again!");
 	return 0;
 } 
